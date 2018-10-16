35571e2be28ba09245343c59fa8365ad5a616be0
iyngaran <dev@iyngaran.info>
2018-10-16 17:11:07 +0530

    formatted the code

--- src/Controller/Factory/SendEmailControllerFactory.php
+++ src/Controller/Factory/SendEmailControllerFactory.php
@@ -19,7 +19,7 @@ use Zend\ServiceManager\Factory\FactoryInterface;
 
 class SendEmailControllerFactory implements FactoryInterface
 {
-    public function __invoke(ContainerInterface $container, array $options = null)
+    public function __invoke(ContainerInterface $container)
     {
         $config = $container->get("Config");
         return new SendEmailController(
